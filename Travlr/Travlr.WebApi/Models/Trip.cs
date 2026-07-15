ce5ddd54dab4c70b1e2f5ceac6e9a9678673a665
= <jard@pathfindercs.com>
2026-07-15 00:34:57 -0400

    null!

--- Travlr/Travlr.WebApi/Models/Trip.cs
+++ Travlr/Travlr.WebApi/Models/Trip.cs
@@ -11,20 +11,20 @@ namespace Travlr.WebApi.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public string Id { get; set; } = null!;
 
         // These properties are required to create a new document
         [BsonRequired]
         [BsonElement("code")]
-        public string Code { get; set; }
+        public string Code { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("name")]
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("length")]
-        public string Length { get; set; }
+        public string Length { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("start")]
@@ -32,21 +32,21 @@ namespace Travlr.WebApi.Models
 
         [BsonRequired]
         [BsonElement("resort")]
-        public string Resort { get; set; }
+        public string Resort { get; set; } = null!;
 
         // price per person stored as a string
         [BsonRequired]
         [BsonElement("perPerson")]
-        public string PerPerson { get; set; }
+        public string PerPerson { get; set; } = null!;
 
         // file path to an image resource
         [BsonRequired]
         [BsonElement("image")]
-        public string Image { get; set; }
+        public string Image { get; set; } = null!;
 
         [BsonRequired]
         [BsonElement("description")]
-        public string Description { get; set; }
+        public string Description { get; set; } = null!;
 
         // __v field is  MongoDB's document version key
         [BsonElement("__v")]
