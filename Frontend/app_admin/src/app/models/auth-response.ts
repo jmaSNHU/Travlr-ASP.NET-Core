4fc027507d7b872a3efe961fec5af61dccba247d
jard <jard@pathfindercs.com>
2026-07-18 15:53:11 -0400

    implemented register component

--- Frontend/app_admin/src/app/models/auth-response.ts
+++ Frontend/app_admin/src/app/models/auth-response.ts
@@ -1,8 +1,12 @@
 export class AuthResponse {
+    isSuccess: boolean;
     token: string;
+    message: string;
 
     constructor() 
     {
+        this.isSuccess = false;
         this.token = '';
+        this.message = '';
     }
 }
